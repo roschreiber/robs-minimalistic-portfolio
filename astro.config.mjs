8871e8917fdee33ab1b9a92307e718f27fc2d868
roschreiber <schreiber.robin@icloud.com>
2025-01-10 20:03:48 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,25 @@
 // @ts-check
-// @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'hybrid', // or 'hybrid' if you prefer
+  integrations: [tailwind(), icon()],
+  
+  vite: {
+    ssr: {
+        noExternal: ['webcoreui']
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler'
+            }
+        }
+    }
+}
+});
